3e94801e187e773f548fffbeb31bb2e0a0bca3b0
pvarper <p.varper@gmail.com>
2019-08-27 23:19:39 -0400

    creacion del test cargarMatrizFilaColumna

--- src/tdd/juego/test/JuegoTest.java
+++ src/tdd/juego/test/JuegoTest.java
@@ -11,11 +11,11 @@ public class JuegoTest {
 	@Test
 	public void cargarMatrizFilaColumna() {
 		Matriz matriz= new Matriz();
-		int inputFila=3;
+		int inputFila=2;
 		int inputColumna=4;
 		int[][]matrizCargada=matriz.cargarMatriz(inputFila, inputColumna);
 		
-		int expectedFila=3;
+		int expectedFila=2;
 		int expectedColumna=4;
 		
 		assertEquals(expectedFila, matrizCargada.length);
